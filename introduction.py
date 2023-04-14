4b68acf9a2faa4f4158f1a64b0b90cd37d078bf7
wshuts <shutswilliam@gmail.com>
2023-04-14 18:11:14 -0500

    exploring frequency and network parameter arrays

--- introduction.py
+++ introduction.py
@@ -6,11 +6,15 @@ class Introduction:
     def run_demo():
         path = 'venv/Lib/site-packages/skrf/data/'
         ring_slot = rf.Network(path + 'ring slot.s2p')
+        print(ring_slot)
 
         short = rf.data.wr2p2_short
         delayshort = rf.data.wr2p2_delayshort
+
         diff = short - delayshort
         quot = short / delayshort
+        print(diff)
+        print(quot)
 
         line = rf.data.wr2p2_line
         delayshort_cascade = line ** short
@@ -20,5 +24,11 @@ class Introduction:
             print("Cascade verified.")
         if short_deembed == short:
             print("De-embed verified.")
+
+        print(type(line.s))
+        print(line.s.shape)
+        print(line.frequency)
+        first_ten_frequencies = line.f[0:10]
+        print(first_ten_frequencies)
 
         return
